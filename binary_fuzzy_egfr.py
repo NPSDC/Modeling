9ea48756292b4d482ba97a4111cdec2ff2166fef
NPSDC <noorpratap@gmail.com>
2015-06-16 13:21:59 +0530

    Added mfs.py and more important are the basic changes in structure of code by changing the if conditions

--- binary_fuzzy_egfr.py
+++ binary_fuzzy_egfr.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import skfuzzy as fuzz
+from mfs import *
 
 def find_index(arr, value):
 	for i in xrange(arr.size):
@@ -19,37 +20,19 @@ def find_closest(arr, value):
 			index = i
 	return index
 
-def eval_membership_functions(initial_values):
-	'''Technically they should have been calcultated using fuzz.someMf but since they are same to save computation we do this'''
-	egf_high = fuzz.gaussmf(initial_values[0], 1, 0.1)
-	egf_low = fuzz.gaussmf(initial_values[0], 0, 0.1)
-	hrg_high = egf_high
-	hrg_low = egf_low
-	egfr_high = egf_high
-	egfr_low =  egf_low
-	erk_high = egfr_high  
-	erk_low = egfr_low 
-	pi3k_high = egfr_high
-	pi3k_low = egfr_low
-	akt_high = egfr_high  
-	akt_low = egfr_low 
-	raf_high = egfr_high
-	raf_low = egfr_low
-	time_high = fuzz.smf(initial_values[7], 0, 1)
-	time_low = fuzz.zmf(initial_values[7], 0, 1)
-	return ((egf_low, egf_high), (hrg_low, hrg_high), (egfr_low, egfr_high), (raf_low, raf_high), (pi3k_low, pi3k_high), (erk_low, erk_high), \
-			(akt_low, akt_high), (time_low, time_high))
+def compute_egfr_change(egf_value, hrg_value, time_value, initial_values, mfs ):	
 
-
-def compute_egfr(egf_value, hrg_value, time_value, initial_values, mfs ):	
-
-	"""Rules---
-	If egf is high or hrg is high and time is high then egfr is high
-	if egf is low and hrg is low or time is low then egfr is low"""
-
+	'''Rules---
+	If egf is high or hrg is high and time is high then positive_change_egfr is high
+	If egf is high or hrg is high and time is low then positive_change_egfr is low
+	if egf is low and hrg is low and time is high then negative_change_egfr is high
+	if egf is low and hrg is low and time is low then negative_change_egfr is low
+	'''
+	####Positive CHange#######
+	####Antecedent1
 	a1_1 = mfs[0][1][initial_values[0] == egf_value] #egf_high[egf == egf_value]
 	a1_2 = mfs[1][1][ initial_values[1] == hrg_value ] #hrg_high[hrg == hrg_value]
-	a1_3 = mfs[3][1][ initial_values[3] == time_value]
+	a1_3 = mfs[3][1][ initial_values[3] == time_value] #time_high[time == time_value]
 
 	if( a1_1.size == 0):
 		a1_1 = mfs[0][1][ find_closest(initial_values[0], egf_value)]
@@ -58,34 +41,61 @@ def compute_egfr(egf_value, hrg_value, time_value, initial_values, mfs ):
 	if( a1_3.size == 0):
 		a1_3 = mfs[3][1][ find_closest(initial_values[3], time_value)]
 
+
 	a1 = max( a1_1, a1_2) 
 	a1 = min(a1, a1_3 )
-	c1 = np.fmin(np.linspace(a1, a1, 100), mfs[2][1])
+	#Consequent 1
+	c1 = np.fmin(np.linspace(a1, a1, mfs[2][3].size), mfs[2][3])  #mfs[2][3] is positve_change_egfr_high
 
-	a2_1 = mfs[0][0][initial_values[0] == egf_value] #egf_low[egf == egf_value]
-	a2_2 = mfs[1][0][initial_values[1] == hrg_value] #hrg_low[hrg == hrg_value]
-	a2_3 = mfs[3][0][initial_values[3] == time_value]
+	####Antecedent2
+	'''Note a2_1 and a2_2 are the same as a1_1 and a1_2 and hence not calculated'''
+	a2_3 = mfs[3][0][ initial_values[3] == time_value] #time_low[time == time_value]
 
-	if( a2_1.size == 0):
-		a2_1 = mfs[0][0][ find_closest(initial_values[0], egf_value)]
-	if( a2_2.size == 0):
-		a2_2 = mfs[1][0][ find_closest(initial_values[1], hrg_value)]
-	if( a2_3.size == 0):
-		a2_3 = mfs[3][0][ find_closest(initial_values[3], hrg_value)]
+	a2 = max( a1_1, a1_2) 
+	a2 = min(a2, a2_3 )
+	#Consequent 2
+	c2 = np.fmin(np.linspace(a2, a2, mfs[2][2].size), mfs[2][2])	#mfs[2][2] is positve_change_egfr_low
 
-	a2 = min(a2_1, a2_2)
-	a2 = max(a2, a2_3)
-	c2 = np.fmin(np.linspace(a2,a2,100), mfs[2][0])
+	c_com_positive = np.fmax(c1, c2)
+	pos_change = fuzz.defuzz(initial_values[2][1], c_com_positive, 'centroid' ) #initial_values[2][1] is positive_change_egfr
 
-	c_com = np.fmax(c1, c2)
-	
-	a =  fuzz.defuzz(initial_values[2], c_com, 'centroid')
-	return a
 
-def compute_raf(egfr_value, akt_value, time_value, initial_values, mfs):
+	#### Negative Change ######
+	####Antecedent 3
+	a3_1 = mfs[0][0][initial_values[0] == egf_value] #egf_low[egf == egf_value]
+	a3_2 = mfs[1][0][initial_values[1] == hrg_value] #hrg_low[hrg == hrg_value]
+	a3_3 = mfs[3][1][initial_values[3] == time_value] #time_high[time == time_value]
+
+	if( a3_1.size == 0):
+		a3_1 = mfs[0][0][ find_closest(initial_values[0], egf_value)]
+	if( a3_2.size == 0):
+		a3_2 = mfs[1][0][ find_closest(initial_values[1], hrg_value)]
+	if( a3_3.size == 0):
+		a3_3 = mfs[3][0][ find_closest(initial_values[3], time_value)]
+
+	a3 = min(a3_1, a3_2, a3_3)
+	#Consequent 3
+	c3 = np.fmin(np.linspace(a3, a3, 100), mfs[2][5]) #mfs[2][5] is negative_change_egfr_high
+
+	'''Note a4_1 and a4_2 are the same as a3_1 and a3_2 and hence not calculated'''
+	a4_3 = mfs[3][0][initial_values[3] == time_value] #time_low[time == time_value]
+	a4 = min(a3_1, a3_2, a4_3)
+
+	#Consequent 4
+	c4 = np.fmin(np.linspace(a4, a4, mfs[2][4].size), mfs[2][4]) #mfs[2][4] is negative_change_egfr_low
+
+	c_com_negative = np.fmax(c3, c4)
+	neg_change = fuzz.defuzz(initial_values[2][2], c_com_negative, 'centroid') #initial_values[2][2] is negative_change_egfr
+
+	return pos_change + neg_change
+
+def compute_raf_change(egfr_value, akt_value, time_value, initial_values, mfs):
 	"""Rules---
-	If egfr is high or akt is high and time is high then raf is high
-	if egfr is low and akt is low  or time is low then raf is low"""	
+	If egfr is high or akt is high and time is high then positive_change_raf is high
+	If egfr is high or akt is high and time is low then positive_change_raf is low
+	if egfr is low and akt is low  and time is low then negative_change_raf is low
+	if egfr is low and akt is low  and time is high then negative_change_raf is high
+	"""
 
 	a1_1 = mfs[0][1][initial_values[0] == egfr_value] #egfr_high[egfr == egfr_value]
 	a1_2 = mfs[1][1][initial_values[1] == akt_value] #akt_high[akt == akt_value]
@@ -121,7 +131,7 @@ def compute_raf(egfr_value, akt_value, time_value, initial_values, mfs):
 	c_com = np.fmax(c1, c2)
 	return fuzz.defuzz(initial_values[2], c_com, 'centroid')
 
-def compute_pi3k(egfr_value, erk_value, time_value, initial_values, mfs):
+def compute_pi3k_change(egfr_value, erk_value, time_value, initial_values, mfs):
 	"""Rules ---
 	if egfr is high and erk is low and time is high then pi3k is high
 	if egfr is low or erk is high or time is low then pi3k is low"""
@@ -157,7 +167,7 @@ def compute_pi3k(egfr_value, erk_value, time_value, initial_values, mfs):
 	c_com = np.fmax(c1, c2)
 	return fuzz.defuzz(initial_values[2], c_com, 'centroid')
 
-def compute_erk(raf_value, time_value, initial_values, mfs):
+def compute_erk_change(raf_value, time_value, initial_values, mfs):
 	"""Rules-
 		If raf is high and time is high erk is high
 		If raf is low or time is low then erk is low"""
@@ -186,7 +196,7 @@ def compute_erk(raf_value, time_value, initial_values, mfs):
 	c_com = np.fmax(c1,c2)
 	return fuzz.defuzz( initial_values[1], c_com, 'centroid')
 
-def compute_akt(pi3k_value, time_value, initial_values, mfs):
+def compute_akt_change(pi3k_value, time_value, initial_values, mfs):
 	"""Rules-
 		If pi3k is high and time is high akt is high
 		If pi3k is low or time is low then akt is low"""
@@ -215,19 +225,26 @@ def compute_akt(pi3k_value, time_value, initial_values, mfs):
 	c_com = np.fmax(c1,c2)
 	return fuzz.defuzz( initial_values[1], c_com, 'centroid')
 
-def check_egfr(not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs):
-	y = np.copy(initial_cond)
-	
-	if 0 in not_updated or 1 in not_updated:
-		
-		y[2] = compute_egfr(initial_cond[0], initial_cond[1], initial_values[7][ time_indexes[0] ], (initial_values[0], initial_values[1], initial_values[2],\
-		 initial_values[7]), (mfs[0], mfs[1], mfs[2], mfs[7]))
-		time_indexes[0] = time_indexes[0] + 1
-		
-	else:
-		time_indexes[0] = 1
+def compute_egfr(change_egfr_reflected, not_updated, initial_cond, time_egfr_index, initial_values, mfs):
 
-	return (y, time_indexes)
+	flag = 0
+	egfr = initial_cond[2]
+
+	if(time_egfr_index > 100):
+		flag = 1
+		change_egfr_reflected = egfr
+	
+	if 0 in not_updated or 1 in not_updated:		
+		time_egfr_index = time_egfr_index + 1		
+	else:
+		flag = 0
+		time_egfr_index = 2
+
+	if flag == 0:
+		egfr = change_egfr_reflected + compute_egfr_change(initial_cond[0], initial_cond[1], initial_values[7][ time_egfr_index - 1], \
+			   (initial_values[0], initial_values[1], initial_values[2], initial_values[7]), (mfs[0], mfs[1], mfs[2], mfs[7]))
+	
+	return (egfr, time_egfr_index, change_egfr_reflected)
 
 def check_raf(y, not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs):
 
@@ -281,49 +298,33 @@ def check_akt(y, not_updated, prev_cond, initial_cond, time_indexes, initial_val
 		time_indexes[4] = time_indexes[4] + 1
 
 	else:
-		time_indexes[4] = 2
-
-
-	
-
-	
+		time_indexes[4] = 2	
 	return (y, time_indexes)
 
 
-def rules(prev_cond, initial_cond, time_indexes, (initial_values, mfs)):	
-	not_updated = []
+def rules(change_reflected, prev_cond, initial_cond, time_indexes, (initial_values, mfs)):	
+	y = np.copy(initial_cond)
+	not_updated = []  #contains the indexes not updated
 	for i in xrange(len(prev_cond)):
 		if str(prev_cond[i]) == str(initial_cond[i]):
 			not_updated.append(i)	
-		'''figure out the break condition'''	
-		if(time_indexes[0] == 158):  
-		#	print  i
-		#	print prev_cond[5] , initial_cond[5]
-		#	break
-			pass
-	#if (time_indexes[0] >= 140 and time_indexes[0] < 145):
-	#		print not_updated,prev_cond,initial_cond
-	y, time_indexes = check_egfr(not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs )
-	#if time_indexes[0] == 142:
-		#print time_indexes
-	y, time_indexes = check_raf(y, not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs )
-	y, time_indexes = check_pi3k(y, not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs )
-	y, time_indexes = check_erk(y, not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs )
-	y, time_indexes = check_akt(y, not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs )
+		
+	y[2], time_indexes[0], change_reflected[2] = compute_egfr(change_reflected[2], not_updated, initial_cond, time_indexes[0], initial_values, mfs )
+	#y, time_indexes = check_raf(y, not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs )
+	#y, time_indexes = check_pi3k(y, not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs )
+	#y, time_indexes = check_erk(y, not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs )
+	#y, time_indexes = check_akt(y, not_updated, prev_cond, initial_cond, time_indexes, initial_values, mfs )
 	#y[5] = initial_cond[3]
 	#y[6] = initial_cond[4]
-	y = np.around(y, 8)
-	return (y,time_indexes)
+	#y = np.around(y, 8)
+	return (y,time_indexes, change_reflected)
 
 def main():
-	#Universal sets
-	#c_egfr_aggregated = compute_initial_egfr(egf, hrg, egfr)
-	#c_raf_aggregated = compute_initial_raf(egfr, akt, raf)
-	#c_pi3k_aggregated = compute_initial_pi3k(egfr, erk, pi3k)
-
+	
 	initial_cond = np.array([1, 1, 0, 0, 0, 0, 0], dtype = "float64")
 	time_stop = 10
 	y = np.copy(initial_cond)
+	change_reflected = np.copy(initial_cond) # This is the array to which we would adding the changing values
 	y.resize(1, 7)
 	step = 1
 
@@ -334,31 +335,42 @@ def main():
 	raf = egf
 	pi3k = egf
 	erk = egf
-	time = np.linspace(0, 10, 1000)
-	vals = (egf, hrg, egfr, raf, pi3k, erk, akt, time)
+
+	positive_change_egfr = egf
+	positive_change_raf = egf
+	positive_change_pi3k = egf
+	positive_change_erk = egf
+	positive_change_akt = egf
+
+	negative_change_egfr = np.linspace(-1, 0, 100)
+	negative_change_raf = negative_change_egfr
+	negative_change_pi3k = negative_change_egfr
+	negative_change_erk = negative_change_egfr
+	negative_change_akt = negative_change_egfr
+
+	time = np.linspace(0, 10, 1001)
+	vals = (egf, hrg, (egfr, positive_change_egfr, negative_change_egfr),\
+		   (raf, positive_change_raf, negative_change_raf),\
+		   (pi3k, positive_change_pi3k, negative_change_pi3k),\
+		   (erk, positive_change_erk, negative_change_erk),\
+		   (akt, positive_change_akt, negative_change_akt), time)
+
 	mfs = eval_membership_functions(vals)
 
-	for i in vals:
-		np.around(i, 8)
-	for i in mfs:
-		np.around(i, 8)
-	#print rules(y[0], 0, (vals,mfs))
-	times = [ 1, 1, 1, 1, 1]
-	for i in xrange(1, time.size):
-			temp, times = rules(y[i-2], y[i - 1], times , (vals, mfs))
-			#print times	
+	times = [ 1, 1, 1, 1, 1] #provides the initial time inputs (indexes 0 for egfr, 1 for raf, 2 for pi3k and so on)
+	for i in xrange(1, time.size ):
+			if(i > 100 and i %100 == 1):
+				y[i - 1][0] = int(y[i - 1][0]) ^ 1
+				y[i - 1][1] = int(y[i - 1][1]) ^ 1
+			temp, times, change_reflected = rules(change_reflected, y[i-2], y[i - 1], times , (vals, mfs))
 			y = np.vstack((y, temp))
+			if i < 201:
+				print change_reflected, times[0], y[i]
+	#print y[98],y[99],y[100],y[101]
 	
-			if i > 90 and i < 200:
-				print 'times',times,'\ny is',y[i]
-			#print time[i ], y[i]
-	#for i in xrange(200,400):
-	#	print y[i]
-	#for i in xrange(400):
-	#	print y[i]
 	plt.title("Synch")
 	
-	lines = plt.plot(time, y[:,4]	)	
+	lines = plt.plot(time, y[:,2]	)	
 	
 	plt.legend(loc='upper right')
 	plt.xlabel('Time')
---
+++ mfs.py
@@ -0,0 +1,84 @@
+import numpy as np
+import skfuzzy as fuzz
+
+'''The various membership functions are computed here'''
+
+def eval_membership_function_egf(egf_values):
+	'''Evaluates membership function for egf '''
+	egf_high = fuzz.gaussmf(egf_values, 1, 0.1)
+	egf_low = fuzz.gaussmf(egf_values, 0, 0.1)
+	return (egf_low, egf_high)
+
+def eval_membership_function_hrg(hrg_values):
+	'''Evaluates membership function for hrg '''
+	hrg_high = fuzz.gaussmf(hrg_values, 1, 0.1)
+	hrg_low = fuzz.gaussmf(hrg_values, 0, 0.1)
+	return (hrg_low, hrg_high)
+
+def eval_membership_function_egfr(egfr_values, positive_change_egfr, negative_change_egfr):
+	'''Evaluates membership function for egfr '''
+	egfr_high = fuzz.gaussmf(egfr_values, 1, 0.1)
+	egfr_low = fuzz.gaussmf(egfr_values, 0, 0.1)
+	positive_change_egfr_high = fuzz.gaussmf(positive_change_egfr, 1, 0.1)
+	positive_change_egfr_low = fuzz.gaussmf(positive_change_egfr, 0, 0.1)
+	negative_change_egfr_high = fuzz.gaussmf(negative_change_egfr, -1, 0.1)
+	negative_change_egfr_low = fuzz.gaussmf(negative_change_egfr, 0, 0.1)
+	return (egfr_low, egfr_high, positive_change_egfr_low, positive_change_egfr_high, negative_change_egfr_low, negative_change_egfr_high)
+
+def eval_membership_function_raf(raf_values, positive_change_raf, negative_change_raf):
+	'''Evaluates membership function for raf '''
+	raf_high = fuzz.gaussmf(raf_values, 1, 0.1)
+	raf_low = fuzz.gaussmf(raf_values, 0, 0.1)
+	positive_change_raf_high = fuzz.gaussmf(positive_change_raf, 1, 0.1)
+	positive_change_raf_low = fuzz.gaussmf(positive_change_raf, 0, 0.1)
+	negative_change_raf_high = fuzz.gaussmf(negative_change_raf, -1, 0.1)
+	negative_change_raf_low = fuzz.gaussmf(negative_change_raf, 0, 0.1)
+	return (raf_low, raf_high, positive_change_raf_low, positive_change_raf_high, negative_change_raf_low, negative_change_raf_high)
+
+def eval_membership_function_pi3k(pi3k_values, positive_change_pi3k, negative_change_pi3k):
+	'''Evaluates membership function for pi3k '''
+	pi3k_high = fuzz.gaussmf(pi3k_values, 1, 0.1)
+	pi3k_low = fuzz.gaussmf(pi3k_values, 0, 0.1)
+	positive_change_pi3k_high = fuzz.gaussmf(positive_change_pi3k, 1, 0.1)
+	positive_change_pi3k_low = fuzz.gaussmf(positive_change_pi3k, 0, 0.1)
+	negative_change_pi3k_high = fuzz.gaussmf(negative_change_pi3k, -1, 0.1)
+	negative_change_pi3k_low = fuzz.gaussmf(negative_change_pi3k, 0, 0.1)
+	return (pi3k_low, pi3k_high, positive_change_pi3k_low, positive_change_pi3k_high, negative_change_pi3k_low, negative_change_pi3k_high)
+
+def eval_membership_function_erk(erk_values, positive_change_erk, negative_change_erk):
+	'''Evaluates membership function for erk '''
+	erk_high = fuzz.gaussmf(erk_values, 1, 0.1)
+	erk_low = fuzz.gaussmf(erk_values, 0, 0.1)
+	positive_change_erk_high = fuzz.gaussmf(positive_change_erk, 1, 0.1)
+	positive_change_erk_low = fuzz.gaussmf(positive_change_erk, 0, 0.1)
+	negative_change_erk_high = fuzz.gaussmf(negative_change_erk, -1, 0.1)
+	negative_change_erk_low = fuzz.gaussmf(negative_change_erk, 0, 0.1)
+	return (erk_low, erk_high, positive_change_erk_low, positive_change_erk_high, negative_change_erk_low, negative_change_erk_high)
+
+def eval_membership_function_akt(akt_values, positive_change_akt, negative_change_akt):
+	'''Evaluates membership function for akt '''
+	akt_high = fuzz.gaussmf(akt_values, 1, 0.1)
+	akt_low = fuzz.gaussmf(akt_values, 0, 0.1)
+	positive_change_akt_high = fuzz.gaussmf(positive_change_akt, 1, 0.1)
+	positive_change_akt_low = fuzz.gaussmf(positive_change_akt, 0, 0.1)
+	negative_change_akt_high = fuzz.gaussmf(negative_change_akt, -1, 0.1)
+	negative_change_akt_low = fuzz.gaussmf(negative_change_akt, 0, 0.1)
+	return (akt_low, akt_high, positive_change_akt_low, positive_change_akt_high, negative_change_akt_low, negative_change_akt_high)
+
+def eval_membership_function_time(time_values):
+	'''Evaluates membership function for time '''
+	time_high = fuzz.smf(time_values, 0, 1)
+	time_low = fuzz.zmf(time_values, 0, 1)
+	return (time_low, time_high)
+
+def eval_membership_functions(initial_values):
+	'''Calls the mfs for all '''
+	egf_mfs = eval_membership_function_egf(initial_values[0])
+	hrg_mfs = eval_membership_function_hrg(initial_values[1])
+	egfr_mfs = eval_membership_function_egfr(initial_values[2][0], initial_values[2][1], initial_values[2][2])
+	raf_mfs = eval_membership_function_raf(initial_values[3][0], initial_values[3][1], initial_values[3][2])
+	pi3k_mfs = eval_membership_function_pi3k(initial_values[4][0], initial_values[4][1], initial_values[4][2])
+	erk_mfs = eval_membership_function_erk(initial_values[5][0], initial_values[5][1], initial_values[5][2])
+	akt_mfs = eval_membership_function_akt(initial_values[6][0], initial_values[6][1], initial_values[6][2])
+	time_mfs = eval_membership_function_time(initial_values[7])
+	return (egf_mfs, hrg_mfs, egfr_mfs, raf_mfs, pi3k_mfs, erk_mfs, akt_mfs, time_mfs)

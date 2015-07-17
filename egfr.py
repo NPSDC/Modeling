82ce55371d623f74bb34eb64256ea7fbaf7d20c6
NPSDC <noorpratap@gmail.com>
2015-07-17 11:50:30 +0530

    Akt also added

--- egfr.py
+++ egfr.py
@@ -51,6 +51,13 @@ def calculate_pi3k_mfs(egfr, erk, pi3k):
 	pi3k_high = fuzz.gaussmf(pi3k, egfr[egfr.size - 1], egfr[egfr.size - 1]/20)
 	return ((egfr_low, egfr_high), (erk_low, erk_high), (pi3k_low, pi3k_high))
 
+def calculate_akt_mfs(pi3k, akt):
+	pi3k_low = fuzz.zmf(pi3k, 0, 0.95)
+	pi3k_high = fuzz.smf(pi3k, 0, 0.9)
+	akt_low = fuzz.gaussmf(akt, 0, akt[akt.size -1]/20)
+	akt_high = fuzz.gaussmf(akt, akt[akt.size -1], akt[akt.size -1]/20)
+	return ((pi3k_low, pi3k_high), (akt_low, akt_high))
+
 def calculate_egfr(time_mfs, egfr_mfs, egfr, time_index):
 	a1 = time_mfs[0][time_index]
 	c1 = np.fmin(a1, egfr_mfs[0])
@@ -104,6 +111,17 @@ def calculate_pi3k(egfr_mfs, erk_mfs, pi3k_mfs, pi3k, egfr_index, erk_index):
 		pi3k_val = 0
 	return pi3k_val
 
+def calaculate_akt(pi3k_mfs, akt_mfs, akt, pi3k_index):
+	a1 = pi3k_mfs[0][pi3k_index]
+	c1 = np.fmin(a1, akt_mfs[0])
+	a2 = pi3k_mfs[1][pi3k_index]
+	c2 = np.fmin(a2, akt_mfs[1])
+	c_com = np.fmax(c1, c2)
+	try:
+		akt_val = fuzz.defuzz(akt, c_com, 'centroid')
+	except AssertionError as e:
+		akt_val = 0
+	return akt_val
 
 def rules(present_values, initial_values, time_indexes, time_length):
 	
@@ -112,6 +130,7 @@ def rules(present_values, initial_values, time_indexes, time_length):
 	egfr_index = np.searchsorted(initial_values[2], present_values[1], 'left')
 	raf_index = np.searchsorted(initial_values[3], present_values[2], 'left')
 	erk_index = np.searchsorted(initial_values[4], present_values[3], 'left')
+	pi3k_index = np.searchsorted(initial_values[5], present_values[4], 'left')
 
 	egfr_mfs = ()
 	raf_mfs = ()
@@ -120,11 +139,11 @@ def rules(present_values, initial_values, time_indexes, time_length):
 
 	if(egf_index > 0):
 		egfr_mfs = calculate_egfr_mfs(initial_values[-1], initial_values[0], initial_values[2], egf_index, time_length[0])
-	if(egfr_index > 0):
-		raf_mfs = calculate_raf_mfs(initial_values[2], initial_values[3])
-		pi3k_mfs = calculate_pi3k_mfs(initial_values[2], initial_values[4], initial_values[5])
-	if(raf_index > 0):
-		erk_mfs = calculate_erk_mfs(initial_values[3], initial_values[4])
+
+	raf_mfs = calculate_raf_mfs(initial_values[2], initial_values[3])
+	pi3k_mfs = calculate_pi3k_mfs(initial_values[2], initial_values[4], initial_values[5])
+	erk_mfs = calculate_erk_mfs(initial_values[3], initial_values[4])
+	akt_mfs = calculate_akt_mfs(initial_values[5], initial_values[6])
 	
 	if(present_values[0] == 0):
 		y[1] = 0		
@@ -143,6 +162,11 @@ def rules(present_values, initial_values, time_indexes, time_length):
 	else:
 		y[3] = calculate_erk((erk_mfs[0], erk_mfs[1]), (erk_mfs[2], erk_mfs[3]), initial_values[4], raf_index)
 
+	if(present_values[4] == 0):
+		y[5] = 0
+	else:
+		y[5] = calaculate_akt((akt_mfs[0][0], akt_mfs[0][1]), (akt_mfs[1][0], akt_mfs[1][1]), initial_values[6], pi3k_index)
+
 	not_updated = []
 	for i in xrange(len(y)):
 		if y[i] == present_values[i]:
@@ -156,20 +180,22 @@ def rules(present_values, initial_values, time_indexes, time_length):
 	return y , time_indexes
 
 def main():
-	y = np.array([0.9, 0, 0, 0, 0])
+	y = np.array([0.9, 0, 0, 0, 0, 0])
 	size = 100
-	time_length = [10, 1.5, 0.5]
+	time_length = [10, 1.0, 1.0]
 	time = np.linspace(0, time_length[0], size*time_length[0] + 1)
 	egf = np.linspace(0, 1, size + 1)
 	egfr = np.linspace(0, y[0], size + 1)
 	raf = egfr
 	pi3k = egfr
+	akt = egfr
 	erk = np.linspace(0, y[0], size + 1)
 	egfr_out = np.zeros(time.size)
 	time_indexes = [1, 1]
 	new_raf_out = np.zeros(time_length[0]*size + 1)
 	new_erk_out = np.zeros(time_length[0]*size + 1)
 	new_pi3k_out = np.zeros(time_length[0]*size + 1)
+	new_akt_out = np.zeros(time_length[0]*size + 1)
 	'''for i in xrange(1,size+1):
 		if i == 0:
 			for t in xrange(1, time.size + 1):
@@ -193,11 +219,12 @@ def main():
 			#time_length = 1
 			plt.axis([-0.01,time_length + .1,-0.01,1.1])
 	plt.show()'''
-	initial_vals = (egf, egf, egfr, raf, erk, pi3k, time)
+	initial_vals = (egf, egf, egfr, raf, erk, pi3k, akt, time)
 	y.resize(1, y.size)
 	time_raf_delay = time_length[1]*size
 	time_erk_delay = time_length[2]*size
 	time_pi3k_delay = time_raf_delay
+	time_akt_delay = time_erk_delay
 
 	if((time_raf_delay%2 == 0 or (time_raf_delay + 1)%2 == 0) and time_erk_delay%2 == 0 or (time_erk_delay+1)%2 == 0):
 		for i in xrange(1, time.size):
@@ -207,6 +234,7 @@ def main():
 			new_raf_out[i] = y[i, 2]
 			new_erk_out[i] = y[i, 3]
 			new_pi3k_out[i] = y[i, 4]
+			new_akt_out[i] = y[i, 5]
 
 			if( i > time_raf_delay ):
 				y[i, 2] = new_raf_out[i - time_raf_delay]
@@ -223,8 +251,13 @@ def main():
 			else:
 				y[i, 3] = 0
 
+			if( i > time_akt_delay):
+				y[i, 5] = new_akt_out[i - time_akt_delay]
+			else:
+				y[i, 5] = 0
+
 			#print y[i]
-		plt.plot(time, y[:,1], time, y[:,3], time, y[:,4])
+		plt.plot(time, y[:,1], time, y[:,5], time, y[:,4])
 		plt.xlabel('Time')
 		plt.ylabel('egfr')
 		plt.axis([-0.1,time_length[0] + 0.01, -0.01, 1.01])
